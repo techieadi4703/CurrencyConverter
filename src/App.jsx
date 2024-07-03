96f28fa58b68ea4417c7e3067043185c795fbd47
techieadi4703 <techie.adi47@gmail.com>
2024-07-03 09:17:43 +0530

    Adding files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import CovertorForm from "./Components/CovertorForm"
+
+const App = () => {
+  return (
+    <div className="currencyConvertor">
+      <h2 className="convertorTitle">Currency-Convertor</h2>
+      <CovertorForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/CovertorForm.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react"
+import CurrencySelect from "./CurrencySelect"
+
+const CovertorForm = () => {
+  const [amount,setAmount]=useState(100);
+  const [fromCurrency,setFromCurrency]=useState("USD");
+  const [toCurrency,setToCurrency]=useState("INR");
+  const [result,setResult]=useState("-")
+  const handleSwapCurrencies =()=>{
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  }
+
+  const getExchangeRate=async()=>{
+    const API_KEY=import.meta.env.VITE_API_KEY;
+    const API_URL=`https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${fromCurrency}/${toCurrency}`;
+  
+    try {
+      const response=await fetch(API_URL);
+      if(!response.ok) throw Error("Something went wrong.!!") 
+
+        const data=await response.json();
+        const rate=(data.conversion_rate*amount).toFixed(2);
+        // console.log(rate);
+        setResult(`${amount} ${fromCurrency}=${rate} ${toCurrency}`);
+      } catch (error) {
+      console.log(error);
+    }
+  
+  }
+
+  const handleFromSubmit=(e)=>{
+    e.preventDefault();
+    getExchangeRate();
+  }
+
+  useEffect(()=>getExchangeRate,[]);
+
+  return (
+    // <div>
+        <form className="convertorForm" onSubmit={handleFromSubmit}>
+        <div className="formGroup">
+          <label className="formLabel">Enter Amount </label>
+          <input type="number" className="formInput" value={amount} onChange={e=>setAmount(e.target.value)} required/>
+        </div>
+        <div className="formGroup formCurrencyGroup">
+
+
+          <div className="formSection">
+            <div className="formLabel">From</div>
+            <CurrencySelect
+              selectedCurrency={fromCurrency}
+              handleCurrency={e=>setFromCurrency(e.target.value)}
+            />
+          </div>
+
+          <div className="swapIcon" onClick={handleSwapCurrencies}>
+            {/* <h2>hello</h2> */}
+            <svg width="16" viewBox="0 0 20 19"   xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M19.13 11.66H.22a.22.22 0 0 0-.22.22v1.62a.22.22 0 0 0 .22.22h16.45l-3.92 4.94a.22.22 0 0 0 .17.35h1.97c.13 0 .25-.06.33-.16l4.59-5.78a.9.9 0 0 0-.7-1.43zM19.78 5.29H3.34L7.26.35A.22.22 0 0 0 7.09 0H5.12a.22.22 0 0 0-.34.16L.19 5.94a.9.9 0 0 0 .68 1.4H19.78a.22.22 0 0 0 .22-.22V5.51a.22.22 0 0 0-.22-.22z"
+                fill="#fff"
+              />
+            </svg>
+          </div>
+
+          <div className="formSection">
+            <div className="formLabel">To</div>
+            <CurrencySelect
+              selectedCurrency={toCurrency}
+              handleCurrency={e=>setToCurrency(e.target.value)}
+            />
+          </div>
+        </div>
+        <button type="submit" className="submitButton">Get Exchange Rate</button>
+        <p className="exchangeRateResult">{result}</p>
+      </form>
+    
+  )
+}
+
+export default CovertorForm
